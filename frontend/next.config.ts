29b85d201b86a0be248435fb861072d471de414a
digitalfred99 <digitalfred99@.com>
2026-09-09 00:29:30 +0000

    adding paystack well know file

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -23,6 +23,21 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
+  async headers() {
+    return [
+      {
+        source:
+          "/.well-known/apple-developer-merchantid-domain-association",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/text",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
